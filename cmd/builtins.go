52ed8b3d0f25d8963c5a817660060cef8bd78b74
Joseph Lewis III <joseph@josephlewis.net>
2021-11-18 20:29:41 -0800

    Added command to track honeypot completion.

---
+++ cmd/builtins.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"fmt"
+	"sort"
+
+	"github.com/spf13/cobra"
+	"josephlewis.net/osshit/commands"
+	"josephlewis.net/osshit/core"
+)
+
+// serveCmd represents the serve command
+var builtinsCmd = &cobra.Command{
+	Use:   "builtins",
+	Short: "Show the builtin commands for the honeypot.",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		var builtins []string
+
+		for path, _ := range commands.AllCommands {
+			builtins = append(builtins, path)
+		}
+
+		for cmd, _ := range core.AllBuiltins {
+			builtins = append(builtins, "shell:"+cmd)
+		}
+
+		sort.Strings(builtins)
+
+		for _, v := range builtins {
+			fmt.Fprintln(cmd.OutOrStdout(), v)
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(builtinsCmd)
+}
