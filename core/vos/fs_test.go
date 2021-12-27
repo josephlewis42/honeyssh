62c11807e9b511c34405e69b08e03536d3b8cb13
Joseph Lewis III <joseph@josephlewis.net>
2021-12-27 10:17:11 -0800

    Cleaned up testing junk

--- core/vos/fs_test.go
+++ core/vos/fs_test.go
@@ -239,64 +239,6 @@ func RunFsTest(t *testing.T, suite FSTestSuite) {
 
 		// TODO: create under a link
 	})
-
-	// // MkdirAll creates a directory path and all parents that does not exist
-	// // yet.
-	// MkdirAll(path string, perm os.FileMode) error
-
-	// no parents exist
-	// partial parents exist
-	// full parents exist
-
-	// // Open opens a file, returning it or an error, if any happens.
-	// Open(name string) (afero.File, error)
-	// Exists
-	// Directory
-	// File
-	// Missing
-	// folder with write permissions, folder with read permissions
-	// TODO symlink
-	// TODO symlink, no follow
-
-	// 	// OpenFile opens a file using the given flags and the given mode.
-	// 	OpenFile(name string, flag int, perm os.FileMode) (afero.File, error)
-
-	// Exists
-	// Directory
-	// Symlink
-	// File
-	// Missing
-
-	// 	// Remove removes a file identified by name, returning an error, if any
-	// 	// happens.
-	// 	Remove(name string) error
-	//    DNE
-	//    directory
-	//    directory w children
-	//
-
-	// 	// RemoveAll removes a directory path and any children it contains. It
-	// 	// does not fail if the path does not exist (return nil).
-	// 	RemoveAll(path string) error
-
-	// 	// Rename renames a file.
-	// 	Rename(oldname, newname string) error
-
-	// 	// Stat returns a FileInfo describing the named file, or an error, if any
-	// 	// happens.
-	// 	Stat(name string) (os.FileInfo, error)
-	//
-	// 	// Chmod changes the mode of the named file to mode.
-	// 	Chmod(name string, mode os.FileMode) error
-	//
-	// 	// Chown changes the uid and gid of the named file.
-	// 	Chown(name string, uid, gid int) error
-	//
-	// 	// Chtimes changes the access and modification times of the named file
-	// 	Chtimes(name string, atime time.Time, mtime time.Time) error
-
-	// TODO: symlinks
-	// Infinite
 }
 
 func TestLinkingFs(t *testing.T) {
